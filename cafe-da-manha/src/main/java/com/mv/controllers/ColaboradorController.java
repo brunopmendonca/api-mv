de460ac0dbc9fa8b50eef5b07d3c85f5db17b908
Bruno Cesar Piovezan Mendonça <62854327+brunopmendonca@users.noreply.github.com>
2021-10-09 05:11:43 -0300

    criando api em java

---
+++ cafe-da-manha/src/main/java/com/mv/controllers/ColaboradorController.java
@@ -0,0 +1,57 @@
+package com.mv.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mv.entities.Colaborador;
+import com.mv.services.ColaboradorService;
+
+@RestController
+@RequestMapping(value = "/colaborador")
+public class ColaboradorController {
+
+	@Autowired
+	private ColaboradorService service;
+
+	@GetMapping(value = "/{cpf}")
+	public ResponseEntity<Colaborador> findByCpf(@PathVariable String cpf) {
+		Colaborador obj = service.findByCpf(cpf);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Colaborador> insert(@RequestBody Colaborador obj) {
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@DeleteMapping(value = "/{cpf}")
+	public ResponseEntity<Void> delete(@PathVariable String cpf) {
+
+		service.delete(cpf);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping
+	public ResponseEntity<Colaborador> update(@RequestBody Colaborador entity) {
+		Colaborador obj = service.update(entity);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Colaborador>> findAll() {
+		List<Colaborador> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
