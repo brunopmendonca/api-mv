0925b600c05d65c6941d8fc5a518aa6adeb8ef9f
brunopmendonca <bcm_1993@hotmail.com>
2021-10-10 18:02:35 -0300

    n

--- cafe-da-manha/src/main/java/com/mv/controllers/ColaboradorController.java
+++ cafe-da-manha/src/main/java/com/mv/controllers/ColaboradorController.java
@@ -4,18 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.mv.entities.Colaborador;
 import com.mv.services.ColaboradorService;
 
+@CrossOrigin(origins = "http://domain2.com", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/colaborador")
 public class ColaboradorController {
@@ -23,18 +17,21 @@ public class ColaboradorController {
 	@Autowired
 	private ColaboradorService service;
 
+	@CrossOrigin
 	@GetMapping(value = "/{cpf}")
 	public ResponseEntity<Colaborador> findByCpf(@PathVariable String cpf) {
 		Colaborador obj = service.findByCpf(cpf);
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@CrossOrigin
 	@PostMapping
 	public ResponseEntity<Colaborador> insert(@RequestBody Colaborador obj) {
 		obj = service.insert(obj);
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@CrossOrigin
 	@DeleteMapping(value = "/{cpf}")
 	public ResponseEntity<Void> delete(@PathVariable String cpf) {
 
@@ -43,12 +40,14 @@ public class ColaboradorController {
 		return ResponseEntity.noContent().build();
 	}
 
+	@CrossOrigin
 	@PutMapping
 	public ResponseEntity<Colaborador> update(@RequestBody Colaborador entity) {
 		Colaborador obj = service.update(entity);
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<Colaborador>> findAll() {
 		List<Colaborador> list = service.findAll();
