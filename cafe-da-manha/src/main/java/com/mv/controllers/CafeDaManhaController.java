ab65df0d03064f29cafa4c986f44f2c28fd73d70
brunopmendonca <bcm_1993@hotmail.com>
2021-10-10 18:42:46 -0300

    alterando controller

--- cafe-da-manha/src/main/java/com/mv/controllers/CafeDaManhaController.java
+++ cafe-da-manha/src/main/java/com/mv/controllers/CafeDaManhaController.java
@@ -4,19 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.mv.entities.CafeDaManha;
 import com.mv.services.CafeDaManhaService;
 
 @RestController
+@CrossOrigin(origins = "http://domain2.com", maxAge = 3600)
 @RequestMapping(value = "/cafe-da-manha")
 public class CafeDaManhaController {
 
